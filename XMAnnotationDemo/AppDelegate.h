018e7a3b5d64d1d025e5fbdb1cc8ef1c64ce0ba5
fq <fuqi@xiaomadada.com>
2018-05-16 17:12:23 +0800

    Initial Commit

---
+++ XMAnnotationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMAnnotationDemo
+//
+//  Created by fuqi on 2018/5/16.
+//  Copyright © 2018年 fuqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XMAnnotationDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XMAnnotationDemo
+//
+//  Created by fuqi on 2018/5/16.
+//  Copyright © 2018年 fuqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
